c0094dd8efbc465ee939132176873e0af8353fbc
tesramaker <tessa.ramaker@student.nhlstenden.com>
2022-03-30 11:12:41 +0200

    Add timefields

--- Admin-Panel-main/src/DocentenPanel/docentPanel.js
+++ Admin-Panel-main/src/DocentenPanel/docentPanel.js
@@ -56,3 +56,159 @@ function changeAvailability() {
         check.setAttribute('checked', true);
     }
 }
+
+function addTimefield(category, day){
+    let list = setIds(category, day);
+    
+    if(list[0] != null){
+        let clone = list[0].cloneNode(true);
+        clone.id = list[3] + 1;
+        list[0].parentNode.appendChild(clone);
+        list[1].style.visibility = 'visible';
+    }
+}
+
+function removeTimefield(category, day){
+    let list = setIds(category, day);
+    let clone = document.getElementById(list[3]+1);
+    
+    if(clone != null){
+        list[0].parentNode.removeChild(clone);
+        list[1].style.visibility = 'invisible';
+    }
+}
+
+function setIds(category, day){
+    let timefield;
+    let minus;
+    let plus;
+    let text;
+    switch(day){
+        case 'monday':
+            switch(category){
+                case 'available':
+                    timefield = document.getElementById('avaMonday');
+                    minus = document.getElementById('mAvaMonday');
+                    plus = document.getElementById('pAvaMonday');
+                    text = 'avaMonday';
+                    break;
+                case 'unavailable':
+                    timefield = document.getElementById('unavaMonday');
+                    minus = document.getElementById('mUnavaMonday');
+                    plus = document.getElementById('pUnavaMonday');
+                    text = 'unavaMonday';
+                    break;
+                case 'absent':
+                    timefield = document.getElementById('preMonday');
+                    minus = document.getElementById('mPreMonday');
+                    plus = document.getElementById('pPreMonday');
+                    text = 'preMonday';
+                    break;
+                default:
+                console.log('Not a category.');
+            }
+            break;
+        case 'tuesday':
+            switch(category){
+                case 'available':
+                    timefield = document.getElementById('avaTuesday');
+                    minus = document.getElementById('mAvaTuesday');
+                    plus = document.getElementById('pAvaTuesday');
+                    text = 'avaTuesday';
+                    break;
+                case 'unavailable':
+                    timefield = document.getElementById('unavaTuesday');
+                    minus = document.getElementById('mUnavaTuesday');
+                    plus = document.getElementById('pUnavaTuesday');
+                    text = 'unavaTuesday';
+                    break;
+                case 'absent':
+                    timefield = document.getElementById('preTuesday');
+                    minus = document.getElementById('mPreTuesday');
+                    plus = document.getElementById('pPreTuesday');
+                    text = 'preTuesday';
+                    break;
+                default:
+                console.log('Not a category.');
+            }
+            break;
+        case 'wednesday':
+            switch(category){
+                case 'available':
+                    timefield = document.getElementById('avaWednesday');
+                    minus = document.getElementById('mAvaWednesday');
+                    plus = document.getElementById('pAvaWednesday');
+                    text = 'avaWednesday';
+                    break;
+                case 'unavailable':
+                    timefield = document.getElementById('unavaWednesday');
+                    minus = document.getElementById('mUnavaWednesday');
+                    plus = document.getElementById('pUnavaWednesday');
+                    text = 'unavaWednesday';
+                    break;
+                case 'absent':
+                    timefield = document.getElementById('preWednesday');
+                    minus = document.getElementById('mPreWednesday');
+                    plus = document.getElementById('pPreWednesday');
+                    text = 'preWednesday';
+                    break;
+                default:
+                console.log('Not a category.');
+            }
+            break;
+        case 'thursday':
+            switch(category){
+                case 'available':
+                    timefield = document.getElementById('avaThursday');
+                    minus = document.getElementById('mAvaThursday');
+                    plus = document.getElementById('pAvaThursday');
+                    text = 'avaThursday';
+                    break;
+                case 'unavailable':
+                    timefield = document.getElementById('unavaThursday');
+                    minus = document.getElementById('mUnavaThursday');
+                    plus = document.getElementById('pUnavaThursday');
+                    text = 'unavaThursday';
+                    break;
+                case 'absent':
+                    timefield = document.getElementById('preThursday');
+                    minus = document.getElementById('mPreThursday');
+                    plus = document.getElementById('pPreThursday');
+                    text = 'preThursday';
+                    break;
+                default:
+                console.log('Not a category.');
+            }
+            break;
+        case 'friday':
+            switch(category){
+                case 'available':
+                    timefield = document.getElementById('avaFriday');
+                    minus = document.getElementById('mAvaFriday');
+                    plus = document.getElementById('pAvaFriday');
+                    text = 'avaFriday';
+                    break;
+                case 'unavailable':
+                    timefield = document.getElementById('unavaFriday');
+                    minus = document.getElementById('mUnavaFriday');
+                    plus = document.getElementById('pUnavaFriday');
+                    text = 'unavaFriday';
+                    break;
+                case 'absent':
+                    timefield = document.getElementById('preFriday');
+                    minus = document.getElementById('mPreFriday');
+                    plus = document.getElementById('pPreFriday');
+                    text = 'preFriday';
+                    break;
+                default:
+                console.log('Not a category.');
+            }
+            break;
+        default:
+            timefield = null;
+            minus = null;
+            plus = null;
+            text = null;
+    }
+    return [timefield, minus, plus, text];
+}
